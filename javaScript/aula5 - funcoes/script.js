5191d80e30cf8a030c7227c230d092f2b145214b
diate.com <diate.rpps@gmail.com>
2026-03-18 21:56:50 -0300

    /Return values

--- javaScript/aula5 - funcoes/script.js
+++ javaScript/aula5 - funcoes/script.js
@@ -1,12 +1,14 @@
 //Makimg my first function
-
+/*
 function greetings(){
     console.log("Hello World!")
 }
 
 greetings()
-
+*/
+//------------------------------------------------------------
 //Parameter and argument
+/*
 function name(userName){
     console.log("ola,", userName)
 }
@@ -18,3 +20,19 @@ function name(userName){
  }
 
  sum(37, 9)
+*/
+//------------------------------------------------------------
+//Return values
+/*
+function sum(a, b){
+    let sum = a + b
+    return sum
+}
+
+let showResult = sum(2, 5)
+console.log(showResult)
+//ou
+console.log(sum(2, 5))
+*/
+//------------------------------------------------------------
+
