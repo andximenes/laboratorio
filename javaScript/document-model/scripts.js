6efff14ebeedc23fe5abcefdb659ac39e168aaad
diate.com <diate.rpps@gmail.com>
2026-03-21 18:22:17 -0300

    alterando estilos

--- javaScript/document-model/scripts.js
+++ javaScript/document-model/scripts.js
@@ -1,3 +1,4 @@
+/*
 //Visualizar conteúdo do document
 console.log(document)
 
@@ -22,3 +23,52 @@ console.log(guestsByClass[0])
 //Acessando elementos pela tag
 let guestByTag = document.getElementsByTagName("li")
 console.log(guestByTag)
+
+//QUERY SELECTOR
+
+//ID
+let getInfo = document.querySelector("#guest-1")
+console.log(getInfo)
+
+//CLASS
+let getInfoByClass = document.querySelector(".guest")
+console.log(getInfoByClass)
+
+*/
+
+//Manipulando conteúdo
+//let content = document.querySelector("#guest-1 span")
+//console.log(content.textContent)
+//content.textContent = "André"
+//console.log(content)
+
+/*
+let content = document.querySelector("#guest-1")
+console.log(content.textContent)//retorna o conteúdo visível e o oculto.
+
+console.log(content.innerText)//retorna somente o conteúdo visível
+
+console.log(content.innerHTML)//retorna o html como texto
+
+//Alterando estilo
+
+*/
+
+let inputName = document.querySelector("#name")
+
+//adiciona a classe
+//inputName.classList.add("input-error")
+
+//remove a classe
+//inputName.classList.remove("input-error")
+
+
+//método Toggle: se o css está ativado via javascript, e usarmos o método toggle e desativa o css e caso o css não estiver aplicado ele ativa o css
+
+//inputName.classList.toggle("input-error")
+
+
+//Modificando as propriedades css do elemento
+
+//let button = document.querySelector("button")
+//button.style.backgroundColor = "red"
