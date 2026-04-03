2cf0abe8f8f2e27d7532b0a025420391bf091ba8
diate.com <diate.rpps@gmail.com>
2026-04-03 17:52:46 -0300

    função que receba 2 números e retorne a soma.

---
+++ javaScript/Eventos/return/js/script.js
@@ -0,0 +1,38 @@
+const firstNumber = document.querySelector("#firstNumber")
+const secondNumber = document.querySelector("#secondNumber")
+const result = document.querySelector("#result")
+const form = document.querySelector("form")
+
+
+//regex substitui/ bloqueia as letras
+function regexConvert (campo) {
+    campo.addEventListener("input", () => {
+        const regex = /\D+/g
+        campo.value = campo.value.replace(regex, "")
+    })
+
+}
+
+//Convertendo
+regexConvert(firstNumber)
+regexConvert(secondNumber)
+
+//função que receba 2 números e retorne a soma.
+function sum (a, b) {
+    return result.textContent = Number(a) + Number(b)
+}
+
+//Usando arrow function somar
+// const sum = (a, b) =>{
+//     return result.textContent = Number(a) + Number(b)
+// }
+
+
+//Mostrando o resultado
+form.addEventListener("submit", (event) => {
+    event.preventDefault()
+
+    sum(firstNumber.value, secondNumber.value)
+})
+
+
