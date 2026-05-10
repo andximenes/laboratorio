ed24c91fa5474569869475d6083d4244ecb41361
diate.com <diate.rpps@gmail.com>
2026-05-10 17:53:46 -0300

    avançando no projeto

--- crud/public/script.js
+++ crud/public/script.js
@@ -2,11 +2,79 @@ const formTarefa = document.getElementById("formTarefa")
 const inputTarefa = document.getElementById("inputTarefa")
 const listaTarefas = document.getElementById("listaTarefas")
 
+//cria uma tarefa
+function adicionarTarefasNaTela(tarefa) {
+    const item = document.createElement("li")
+    item.classList.add("tarefa-item")
+    item.innerHTML = `
+        <div class="tarefa-info">
+            <span class="tarefa-titulo">${tarefa.titulo}</span>
+            <span class= "tarefa-status">${tarefa.concluida === 1 ? "Concluída" : "Pendente"}</span>
+        </div>
+        <div class= "tarefa-acoes">
+            <button>Concluir</button>
+            <button onclick= "excluirTarefa(${tarefa.id}, this)">Excluir</button>
+        </div>
+    `
+    listaTarefas.appendChild(item) //coloca a li que criamos dentro da ul (pai) para aparecer na tela
+}
 
-formTarefa.addEventListener("submit", (event) => {
+//lê (mostra) as tarefas criadas
+async function carregarTarefas() {
+    const resposta = await fetch("/tarefas") //vá até a rota /tarefas e espere a resposta da API. Quando chegar a resposta, armazene na variável resposta.
+
+    const tarefas = await resposta.json() //pegue o JSON que veio da API e transforme em dados JavaScript. Armazene esses dados na variável tarefas. Nossa API retorna um array de tarefas, então a variável tarefas vai conter esse array.
+
+    //para cada item dentro do array tarefas, pegue esse item, chame ele temporariamente de tarefa, e execute a função adicionarTarefasNaTela passando essa tarefa. Assim, cada tarefa do array vai ser adicionada na tela.
+    tarefas.forEach((tarefa) => {
+        adicionarTarefasNaTela(tarefa)
+    })
+}
+
+//exclui uma tarefa
+async function excluirTarefa(id, botao) {
+    //envie uma requisição para a API pedindo para excluir a tarefa com esse id.
+    const resposta = await fetch(`/tarefas/${id}`, {
+        method: "DELETE"
+    })
+
+    //se não ok, ou seja, se for diferente de bem sucedida
+    if(!resposta.ok) {
+        alert("Erro ao excluir tarefa")
+        return
+    }
+    //a partir desse botão, suba no HTML até encontrar o <li> mais próximo, que é o item da tarefa, e remova ele da tela.
+    const item = botao.closest("li")
+    item.remove()
+}
+
+formTarefa.addEventListener("submit", async (event) => {
     //impede a página de recaregar quando enviamos uma tarefa
     event.preventDefault()
 
     const titulo = inputTarefa.value
-    console.log(`Titulo digitado: ${titulo}`)
+
+    const resposta = await fetch("/tarefas", {
+        method: "POST",
+        headers: {
+            "content-type": "application/json"
+        },
+        body: JSON.stringify({ 
+            titulo: titulo
+        })
+    })
+
+    const tarefaCriada = await resposta.json()
+
+    if (!resposta.ok) {
+        alert(tarefaCriada.mensagem || "Erro ao criar tarefa")
+        return
+    }
+    
+    adicionarTarefasNaTela(tarefaCriada)
+    inputTarefa.value = ""
+
 })
+
+//mostra as tarefas criadas na tela quando a página é carregada
+carregarTarefas()
--- crud/server.js
+++ crud/server.js
@@ -2,17 +2,12 @@
 
 const express = require('express')
 const tarefasRouter = require("./routes/tarefas")
-const app = express()
 
-app.get("/", (req, res) => {
-    res.json({
-        mensagem: "Bem-vindo à API de Tarefas! 🚀"
-    })
-})
+const app = express()
 
 app.use(express.json())
 
-app.use.static("public")// express servindo os arquivos estáticos da pasta "public". Isso significa que qualquer arquivo colocado nessa pasta pode ser acessado diretamente pelo navegador, como por exemplo: http://localhost:3000/arquivo.html. É uma maneira fácil de servir arquivos como HTML, CSS, JavaScript, imagens, etc., sem precisar criar rotas específicas para cada um deles.
+app.use(express.static("public"))// express servindo os arquivos estáticos da pasta "public". Isso significa que qualquer arquivo colocado nessa pasta pode ser acessado diretamente pelo navegador, como por exemplo: http://localhost:3000/arquivo.html. É uma maneira fácil de servir arquivos como HTML, CSS, JavaScript, imagens, etc., sem precisar criar rotas específicas para cada um deles.
 
 //Tudo que começa com "/tarefas" será tratado pelo tarefasRouter, que é importado do arquivo routes/tarefas.js. Isso permite organizar as rotas relacionadas às tarefas em um módulo separado, mantendo o código mais limpo e modularizado.
 app.use("/tarefas", tarefasRouter)
