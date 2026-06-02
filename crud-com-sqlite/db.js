71696d8a5e8088373d217038a6129cfe9afed339
diate.com <diate.rpps@gmail.com>
2026-06-02 16:07:25 -0300

    criaçãod o creatAt, ou seja, para sabermos a data e a hora da criação

--- crud-com-sqlite/db.js
+++ crud-com-sqlite/db.js
@@ -16,7 +16,8 @@ db.serialize(() => {
         `CREATE TABLE IF NOT EXISTS tasks (
             id INTEGER PRIMARY KEY AUTOINCREMENT,
             title TEXT NOT NULL,
-            isCompleted INTEGER NOT NULL DEFAULT 0
+            isCompleted INTEGER NOT NULL DEFAULT 0,
+            createdAt TEXT NOT NULL CURRENT_TIMESTAMP
         )`
     )
 })
