b5b255c0abb8ecc41167d7ada0ba365da4658616
Ricardo Andre Ximenes Dos Santos <andre.ximenes@previdencia.gov.br>
2026-09-16 16:11:16 -0300

    calc teste js

---
+++ javaScript/calc-testes/js/index.js
@@ -0,0 +1,6 @@
+let display = document.getElementById("display")
+let numbers = document.querySelectorAll(".number")
+let operators = document.querySelectorAll(".operator")
+
+const equal = document.getElementById("equal")
+const clear = document.getElementById("clear")
